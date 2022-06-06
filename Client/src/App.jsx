b5022ddfd67f074e2234b27d9ac8b8e82f2204ff
Neelam <neelam0101singh@gmail.com>
2022-06-06 12:42:35 +0530

    fisrt commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      hello neelam
+    </div>
+  )
+}
+
+export default App
---
+++ Server/src/index.js
@@ -0,0 +1,23 @@
+const express=require("express")
+const app=express()
+
+const cors=require("cors")
+app.use(cors())
+app.use(express.json())
+const mongoose=require("mongoose")
+const connect=()=>{
+    return mongoose.connect("mongodb+srv://Neelam0101:Neelam12345@cluster0.yca2t.mongodb.net/beardo")
+}
+
+const port=process.env.PORT||8080
+
+
+app.listen(port,async()=>{
+    try{
+      await connect()
+    }
+    catch(error){
+        console.log(error)
+    }
+console.log(`listening on port ${port}`)
+} )
